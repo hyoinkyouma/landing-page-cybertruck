f7ba8896e33fee3d2ed6c27f1af3c88d7f67c216
Roman Cabalum <roman.cabalum@gmail.com>
2022-04-26 19:06:37 +0800

    merging into master

--- src/creds.js
+++
@@ -1,7 +0,0 @@
-//ayy lmao if this is on github made big dodoo.
-const mailchimp = {
-  apiKey: "7c9e01bcb821e15c772a7cb30042cc17-us14",
-  listID: "ff35c83c20",
-};
-
-module.exports = { mailchimp };
