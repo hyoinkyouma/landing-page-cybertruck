cdd3c7b7b4d5ac98330fdfc8069d0f3f01b69ed5
Roman Cabalum <roman.cabalum@gmail.com>
2022-04-26 17:54:18 +0800

    changed api keys

--- src/creds.js
+++ src/creds.js
@@ -1,6 +1,6 @@
 //ayy lmao if this is on github made big dodoo.
 const mailchimp = {
-  apiKey: "bbd10e4dae5a05c91e4ace5d030a6b91-us14",
+  apiKey: "7c9e01bcb821e15c772a7cb30042cc17-us14",
   listID: "ff35c83c20",
 };
 
